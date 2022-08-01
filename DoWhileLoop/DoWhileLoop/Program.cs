906aa89b9fbee1fd3bad5458ff86c3adc6bccd42
Zeynep Ceyhan <104215922+zeynepnceyhan@users.noreply.github.com>
2022-08-01 18:05:03 +0300

    Do While loop
    internship

---
+++ DoWhileLoop/DoWhileLoop/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+            do
+            {
+                Console.WriteLine(number);
+                number--;
+            } while (number >= 0);
+            Console.ReadLine();
+        }
+    }
+}
